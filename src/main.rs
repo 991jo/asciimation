1cbb2915c3f9be37abec3a20599c3bd1be7d0e4a
Imarok <Imarok@users.noreply.github.com>
2024-07-07 17:22:10 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ pub mod frame;
 fn main() {
     let mut last_step = time::Instant::now();
     let step_length = time::Duration::from_millis(16);
-    let mut animation= RandomWalkers::default();
+    let mut animation = RandomWalkers::default();
     let should_run = Arc::new(AtomicBool::new(true));
     ctrlc::set_handler({
         let should_run = should_run.clone();
